e0e6915a1027b42de04a0fb5ede486b2372e961e
Brent Acs <brent.mann@gmail.com>
2022-01-18 18:20:06 -0500

    Empty project w/ instructions.

---
+++ ProjectOne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 1. defines a class Person, w/ 4 properties.
+//    FirstName (string)
+//    LastName (string)
+//    Age (int)
+//    Salary (floating)
+//
+// 2. create a collection of Person.
+// 3. insert 5 new persons.
+// 4. display collection to console
+// 5. then sort it.
+// 6. display collection to console (again)
+
+namespace ProjectOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
